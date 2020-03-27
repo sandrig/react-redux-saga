fc47170d6aaeb54b9c4cda1b478e6cea914b2ea6
sandrig <sandrig.dev@gmail.com>
2020-03-27 11:26:47 +0300

    feat: add template by condition

--- src/components/fetched-posts.js
+++ src/components/fetched-posts.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Post } from './post'
 
-export const FetchedPosts = () => {
-  return (
-    <div>
-      <h2>Fetched posts</h2>
-    </div>
-  )
+export const FetchedPosts = ({ posts }) => {
+  if (!posts.length) {
+    return <button className="btn btn-primary">Загрузить</button>
+  }
+  return posts.map(post => <Post post={post} key={post} />)
 };
