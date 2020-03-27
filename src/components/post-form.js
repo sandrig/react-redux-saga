bbb6e9d20c7ecd54fa1dc44478acc5466a1fff69
sandrig <sandrig.dev@gmail.com>
2020-03-27 11:20:48 +0300

    feat: add post form component

---
+++ src/components/post-form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class PostForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Post form</h2>
+      </div>
+    )
+  }
+}
