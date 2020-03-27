99d419fbc8faf67e2fcce0633fca447ff3e46ef9
sandrig <sandrig.dev@gmail.com>
2020-03-27 11:22:42 +0300

    feat: add base template structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { PostForm } from './components/post-form';
+import { PostList } from './components/post-list';
+import { FetchedPosts } from './components/fetched-posts';
 
 function App() {
   return (
     <div className="container">
-      <h1>React</h1>
+      <div className="row">
+        <div className="col">
+          <h1>React Redux Saga</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>Синхронные посты</h2>
+          <PostList posts={[1, 2, 3]} />
+        </div>
+        <div className="col">
+          <h2>Асинхронные посты</h2>
+          <FetchedPosts posts={[]} />
+        </div>
+      </div>
     </div>
   );
 }
