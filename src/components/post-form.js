0bcf7bf1b4543352c67344f03f199973c0d90f52
sandrig <sandrig.dev@gmail.com>
2020-03-29 17:28:04 +0300

    feat: add post form component

--- src/components/post-form.js
+++ src/components/post-form.js
@@ -4,14 +4,48 @@ export class PostForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      title: ''
+    }
   }
+
+  submitHandler = event => {
+    event.preventDefault();
+
+    const { title } = this.state;
+
+    const newPost = {
+      title, id: Date.now().toString()
+    };
+
+    this.setState({ title: '' });
+  };
+
+  changeInputHandler = event => {
+    event.persist();
+    this.setState(prev => ({
+      ...prev, ...{
+        [event.target.name]: event.target.value
+      }
+    }))
+  };
 
   render() {
     return (
-      <div>
-        <h2>Post form</h2>
-      </div>
+      <form onSubmit={this.submitHandler}>
+        <div className="form-group">
+          <label htmlFor="title">Заголовок поста</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeInputHandler}
+          />
+        </div>
+        <button className="btn btn-success" type="submit">Создать</button>
+      </form>
     )
   }
 }
