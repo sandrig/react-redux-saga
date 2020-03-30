aa8ec5d875863d05e9400a3129e9aaa7263356eb
sandrig <sandrig.dev@gmail.com>
2020-03-30 15:44:32 +0300

    feat: add redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './redux/rootReducer';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+render(app, document.getElementById('root'));
